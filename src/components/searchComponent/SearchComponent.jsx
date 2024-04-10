39670879515e7846392db01ed59048b85d312e9e
vijayvp09 <vvpillai95@gmail.com>
2024-04-10 18:51:07 +0530

    change in responsiveness

--- src/components/searchComponent/SearchComponent.jsx
+++ src/components/searchComponent/SearchComponent.jsx
@@ -13,7 +13,7 @@ const SearchComponent = () => {
         
             <form onSubmit={handleSubmit} className="w-full flex justify-center items-center">
                 <input type="text" placeholder="search " name="searchTerm" className="rounded-lg lg:w-1/4 w-44 h-8 appearance-none focus:outline-none px-3 text-xl pb-1 mt-2" />
-                <button type="submit" className="ml-3 bg-gray-800 rounded-lg w-16 lg:w-20 h-8 mt-2" >Search</button>
+                <button type="submit" className="ml-3 bg-gray-400 rounded-lg w-16 lg:w-20 h-8 mt-2" >Search</button>
             </form>
         
     )
